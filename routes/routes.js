581d3f54fc0604c282d47494e35a31f96900a1b7
Mira Keränen <mira.keranen@aalto.fi>
2020-12-03 16:04:53 +0200

    Added validation of reports and adding to database

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,10 @@
 import { Router } from "../deps.js";
-import { hello, morning } from "./controllers/helloController.js";
+import { hello, showMorningReport, submitMorningReport } from "./controllers/morningController.js";
 
 const router = new Router();
 
 router.get('/behavior/reporting', hello);
-router.get('/behavior/reporting/morning', morning);
+router.get('/behavior/reporting/morning', showMorningReport);
+router.post('/behavior/reporting/morning', submitMorningReport);
 
 export { router };
